7af0090f94192db953077c7295584205e52a0377
Juarez <ujuarez2@cps.k12.il.us>
2024-08-30 09:23:25 -0500

    first commit

---
+++ lesson_2.py
@@ -0,0 +1,50 @@
+# # comments are used to explain the code
+# # this is a comment it will not be executed
+# # comments are notes to the programmer
+# # print function allows you to print a messge to the console
+
+# print("day 3 python programming")
+
+# # print function allows us to make an output to the console
+# # input function allows you to get input from the user
+# # input function will return a string
+
+# # name = input("What is your name?")
+# # print("Hello " + name)
+
+# # name is the variable that will store the input from the user
+# # a variable is a container that stores data
+
+# #ask the user for their age
+# age = input("How old are you?")
+# #print out the age of the user with their name
+# print("Oh so you are" + age + " years old")
+
+# # concatenation is the process of joining strings together
+# # with a variable using a plus sign
+
+# #ask for their home address
+# home_address = input("Where do you live?")
+# #print out the home address of the user
+# print("So you live at" + home_address + ".")
+
+# #ask for their phone number
+# phone_number = input("What is your phone number?")
+# #print out the phone number of the user
+# print("So your number is" + phone_number + ".")
+
+# #ask for their email address
+# email_address = input("What is your email address?")
+# #print out their email address
+# print("So your email address is" + email_address)
+
+# data types variables can store different types of data
+# strings are text data -- string data type
+name = "Ulysses" #whenever you see text in quotes it is a string
+# integers are whole numbers
+age = 15 #no quotes around the number
+# floats are decimal numbers
+height = 5.7 #a number with a decimal point
+# ooolean are true or false values
+is_student = True #True or False
+# lists are a collection of data
